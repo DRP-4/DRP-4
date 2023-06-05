8f6b4c554e9e6e7166eb3344f1a5e128846b3f8b
Iurii Zamiatin <iz321@ic.ac.uk>
2023-06-05 12:44:34 +0100

    frontend: Fix indentation

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,10 +2,9 @@ const API_BASE = import.meta.env.DEV
   ? "http://127.0.0.1:5000"
   : "https://drp-04.herokuapp.com/";
 
-
 function newAPIRequest(route: String): Request {
   // Include credentials for the cookie to be included
-  return new Request(`${API_BASE}/api/${route}`, { credentials: 'include' });
+  return new Request(`${API_BASE}/api/${route}`, { credentials: "include" });
 }
 
 export async function get(route: string): Promise<any> {
@@ -18,7 +17,7 @@ export async function post(route: string, value: any): Promise<Response> {
   const response = await fetch(newAPIRequest(route), {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body,
   });
