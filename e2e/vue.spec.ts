bab3fe70f12a64193be62cb010bbd71938055268
Iurii Zamiatin <iz321@ic.ac.uk>
2023-06-12 01:18:59 +0100

    e2e: Update app root test

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -6,6 +6,6 @@ test("visits the app root url", async ({ page }) => {
   await page.goto("/");
   await expect(page.locator("input[type=text]")).toHaveAttribute(
     "placeholder",
-    "New task..."
+    "Task name (required)"
   );
 });
