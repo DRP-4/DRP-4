561c56ce4a76ffa9bc4b8399c361de886d8665ae
Alona Enraght-Moony <code@alona.page>
2023-06-16 00:11:45 +0100

    Don't have leading `/` in api routes

--- src/api/debug.ts
+++ src/api/debug.ts
@@ -3,10 +3,10 @@ import { store } from "@/stores/time_jump";
 
 export async function jumpSeconds(seconds: number) {
   store.jump(seconds);
-  return await post("/debug/jump-seconds", { seconds: store.deltaInSeconds });
+  return await post("debug/jump-seconds", { seconds: store.deltaInSeconds });
 }
 
 export async function resetJumpCounter() {
   store.jump(0);
-  return await post("/debug/jump-seconds", { seconds: 0 });
+  return await post("debug/jump-seconds", { seconds: 0 });
 }
