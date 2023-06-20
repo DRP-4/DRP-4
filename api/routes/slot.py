bc649e58d1c7272d0ddb3917f6902bfb8854f351
Daniel G <code@danielg0.com>
2023-06-20 13:28:08 +0100

    Rework review slot to be absolute
    Rather than be an offset of the current break end, modify the review
    slot method to set it as a constant value added onto the work slot end,
    which doesn't change. This makes implementing multi-users easier.

--- api/routes/slot.py
+++ api/routes/slot.py
@@ -25,7 +25,7 @@ def review_slot(user_id):
     if feedback < 1 or feedback > 3:
         return "Bad Request", 400
 
-    # check this slot is ours, and that the end has passed
+    # post feedback for session
     new_dur = (
         update(Slot)
         .where(Slot.user_id == user_id)
@@ -34,59 +34,49 @@ def review_slot(user_id):
     )
     db.session.execute(new_dur)
 
-    # if the session went badly or well, adjust the break period
-    # add on 5 minutes if reacted poorly, take off 5 mins if good
-    # adjust session after as well
-    if feedback == 1 or feedback == 3:
-        # get this slots end time
-        this_slot_q = (
-            select(Slot).where(Slot.user_id == user_id).where(Slot.slot_id == slot_id)
-        )
-        this_slot = db.session.execute(this_slot_q).scalars().first()
-        if this_slot is not None:
-            next_break_q = (
-                select(Slot)
-                .where(Slot.user_id == user_id)
-                .where(Slot.start == this_slot.end)
-            )
-            next_break = db.session.execute(next_break_q).scalars().first()
-        if next_break is not None:
-            next_work_q = (
-                select(Slot)
-                .where(Slot.user_id == user_id)
-                .where(Slot.start == next_break.end)
-            )
-            next_work = db.session.execute(next_work_q).scalars().first()
+    # set the break period based on feedback
+    # 10 mins if reacted positively
+    # 15 mins if reacted meh
+    # 20 mins if reacted negatively
 
-        # satisfied is 1, dissatisfied is 3
-        satis_delta = datetime.timedelta(seconds=600)
-        if next_break is not None:
-            ch_break = (
-                update(Slot)
-                .where(Slot.slot_id == next_break.slot_id)
-                .values(
-                    end=(
-                        (next_break.end - satis_delta)
-                        if (feedback == 1)
-                        else (next_break.end + satis_delta)
-                    )
-                )
-            )
-            db.session.execute(ch_break)
-        if next_work is not None:
-            ch_work = (
-                update(Slot)
-                .where(Slot.slot_id == next_work.slot_id)
-                .values(
-                    start=(
-                        (next_work.start - satis_delta)
-                        if (feedback == 1)
-                        else (next_work.start + satis_delta)
-                    )
-                )
-            )
-            db.session.execute(ch_work)
+    # find the next break and work slot (if they exist)
+    this_slot_q = (
+        select(Slot).where(Slot.user_id == user_id).where(Slot.slot_id == slot_id)
+    )
+    this_slot = db.session.execute(this_slot_q).scalars().first()
+    if this_slot is None:
+        return "Bad Request - slot not found", 400
+    if not this_slot.work:
+        return "Bad Request - slot not a work slot", 400
+    next_break_q = (
+        select(Slot).where(Slot.user_id == user_id).where(Slot.start == this_slot.end)
+    )
+    next_break = db.session.execute(next_break_q).scalars().first()
+    if next_break is not None:
+        next_work_q = (
+            select(Slot)
+            .where(Slot.user_id == user_id)
+            .where(Slot.start == next_break.end)
+        )
+        next_work = db.session.execute(next_work_q).scalars().first()
+
+    # set break length based on feedback
+    # feedback: 1=satisfied, 2=neutral, 3=dissatisfied, null=ungiven
+    five_mins = datetime.timedelta(seconds=300)
+    breaklength = five_mins + this_slot.feedback * five_mins
+    breakend = this_slot.end + breaklength
+
+    if next_break is not None:
+        ch_break = (
+            update(Slot).where(Slot.slot_id == next_break.slot_id).values(end=breakend)
+        )
+        db.session.execute(ch_break)
+
+    if next_work is not None:
+        ch_work = (
+            update(Slot).where(Slot.slot_id == next_work.slot_id).values(start=breakend)
+        )
+        db.session.execute(ch_work)
 
     db.session.commit()
-
     return body
